c63aa02479392c17ec67cceb9b17a73c7a5a07a8
mistermur <brian.j.murillo@gmail.com>
2019-02-12 09:32:19 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
 # Enter your procedural solution here!
+def sum_multiples(max)
+  sum = 0
+  (1...max).each do |n|
+    # binding.pry
+    if (n%3==0) || (n%5==0)
+      sum += n
+      # byebug
+      # binding.pry
+    end
+  end
+  # binding.pry
+  return sum
+end
+
+
+def collect_multiples(max)
+  arr_sum = []
+  (1...max).each do |n|
+    if (n%3==0) || (n%5==0)
+      arr_sum << n
+    end
+  end
+  arr_sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+  def collect_multiples()
+    arr_sum = []
+    (1...@limit).each do |n|
+      if (n%3==0) || (n%5==0)
+        arr_sum << n
+      end
+    end
+    arr_sum
+  end
+
+  def sum_multiples()
+    sum = 0
+    (1...@limit).each do |n|
+      if (n%3==0) || (n%5==0)
+        sum += n
+      end
+    end
+    return sum
+  end
+
+end
