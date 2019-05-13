f37191f781a332b2df434ce93af102cb7588a84a
github username <github email address>
2019-05-13 12:26:41 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+
+def findAllNums
+  array = []
+  x = 0
+  while x < 1000 do
+   three = x * 3
+   if !array.include?(three)
+     array << three
+   end
+  five = x * 5
+ if !array.include?(five)
+   array << five
+ end
+   x += 1
+ end
+ num = 0
+ array.each do |i|
+   num += i
+ end
+ return num
+end
+findAllNums()
